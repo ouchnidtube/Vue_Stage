8e733a9a9940253720977a0bee9c754502eed9a9
Yassine OUCHNID <44616623+ouchnidtube@users.noreply.github.com>
2022-09-27 17:18:56 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <header-top></header-top>
+  <router-view/>
+</template>
+
+<script>
+  import HeaderTop from './components/HeaderTop.vue';
+ 
+  export default {
+    name: 'App',
+    components: {
+      HeaderTop,
+    }
+  }
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+body{
+  overflow-x: hidden;
+}  
+</style>
